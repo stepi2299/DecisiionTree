5fc03afd678b79cae28f96e1b206f00374b0da40
stepi2299 <boenisch22@gmail.com>
2023-01-14 10:28:42 +0100

    creating penalty class, filling Tree attributes like depth, max depth etc, new metrics (classicifaction reptort) and fixed evaluating for different data types as labels

--- cw4.py
+++ cw4.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
 from math import e, log
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 
 from typing import Tuple, Dict, Union
+
+THRESHOLD = 0.6
 
 
 class Attribute:
@@ -30,6 +32,10 @@ class Node:
 class Tree:
     def __init__(self):
         self.model = None
+        self.depth = 0
+        self.nodes_count = 0
+        self.depth_max = None
+        self.nodes_max = 0
 
     @staticmethod
     def calculate_entropy(classes: np.ndarray, base=e) -> float:
@@ -87,7 +93,7 @@ class Tree:
         :param examples:
         :param classes:
 
-        :return: index of chosen attribute
+        :return: index of chosen attribute and value of max information gain
         """
         whole_input_entropy = Tree.calculate_entropy(classes)
         attributes_gains = []
@@ -97,7 +103,7 @@ class Tree:
                     whole_input_entropy, attribute, i, examples, classes
                 )
             )
-        return np.argmax(attributes_gains)
+        return np.argmax(attributes_gains), np.max(attributes_gains)
 
     @staticmethod
     def getting_indexes_of_examples_with_specific_value(
@@ -115,8 +121,8 @@ class Tree:
     def calculate_class_frequency(classes: np.ndarray) -> np.ndarray:
         return np.unique(classes, return_counts=True)[1]
 
-    @staticmethod
     def preprocess_input_dataset(
+            self,
         input_dataset: pd.DataFrame, class_idx=-1, id_idx=None, bins_nr=5
     ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         columns_to_drop = [input_dataset.columns[class_idx]]
@@ -132,7 +138,11 @@ class Tree:
             processed_data = examples[examples.columns[inx]]
             pos_value = len(processed_data.unique())
             if pos_value > 20:
+                self.nodes_max += bins_nr
                 examples[examples.columns[inx]] = pd.cut(processed_data, bins=bins_nr, labels=np.arange(bins_nr))
+            else:
+                self.nodes_max += pos_value
+        self.nodes_max *= tmp_attributes.size-1
         examples = examples.to_numpy(dtype=np.dtype("object"))
         for idx, name in enumerate(tmp_attributes):
             attributes[idx] = Attribute(name, np.unique(examples[:, idx]))
@@ -144,15 +154,22 @@ class Tree:
         attributes, all_examples, all_classes = self.preprocess_input_dataset(
             dataset, class_idx, id_idx, bins_nr
         )
+        self.depth_max = len(attributes)
+        self.leafs = 0
         examples, t_examples = train_test_split(all_examples, test_size=0.4, random_state=2, shuffle=True)
         classes, t_classes = train_test_split(all_classes, test_size=0.4, random_state=2, shuffle=True)
 
-        self.model = Tree._train(attributes, examples, classes, 1)
+        self.model = self._train(attributes, examples, classes, 1)
+        print("nodes", self.nodes_count)
+        print("depth", self.depth)
+        print("leafs", self.leafs)
+        print("depth_max", self.depth_max)
+        print("nodes_max", self.nodes_max)
 
         self.evaluate(t_examples, t_classes)
 
-    @staticmethod
     def _train(
+        self,
         attributes: np.ndarray,
         examples: np.ndarray,
         classes: np.ndarray,
@@ -160,20 +177,30 @@ class Tree:
     ):
         # TODO protect from empty input data
         if examples.size == 0:
+            self.leafs += 1
             return Leaf(most_common_label)
         min_label = np.min(classes)
         max_label = np.max(classes)
         if min_label == max_label:
+            self.leafs += 1
             return Leaf(min_label)
         elif attributes.size == 0:
+            self.leafs += 1
             return Leaf(Tree.calculate_most_common_label(classes))
         else:
             most_common_label = Tree.calculate_most_common_label(classes)
-            attr_idx = Tree.deciding_to_division_attribute(
+            attr_idx, attribute_gain = Tree.deciding_to_division_attribute(
                 attributes, examples, classes
             )
             best_attribute = attributes[attr_idx]
-            new_node = Node(best_attribute,index=attr_idx)
+            current_depth = self.depth_max-len(attributes) + 1
+            self.depth = max(self.depth, current_depth)
+            if attribute_gain >= self.penalty_function(current_depth):
+                new_node = Node(best_attribute, index=attr_idx)
+            else:
+                self.leafs += 1
+                return Leaf(most_common_label)
+            self.nodes_count += 1
             new_attributes = np.delete(attributes, attr_idx)
             new_examples = np.delete(examples, attr_idx, axis=1)
             for value in new_node.attribute.possible_values:
@@ -184,41 +211,54 @@ class Tree:
                 )
                 truncated_classes = np.take(classes, value_occurred_idxes)
                 truncated_examples = np.take(new_examples, value_occurred_idxes, axis=0)
-                new_node.child_nodes[value] = Tree._train(
+                new_node.child_nodes[value] = self._train(
                     new_attributes,
                     truncated_examples,
                     truncated_classes,
                     most_common_label,
                 )
+
         return new_node
 
     def predict(self, example: np.ndarray) -> int:
+        tmp = 0
         model = self.model
         if len(example.shape) > 1 and example.shape[1] == 1:
             raise Exception(f"Wrong input size, to predict must be single example")
         else:
             while model.node_name != "leaf":
+                tmp += 1
                 anazyzed_value = example[model.index]
                 example = np.delete(example, model.index)
                 model = model.child_nodes[anazyzed_value]
             return model.value
 
-    def penalty_function(self):
-        pass
+    def penalty_function(self, current_depth: int):
+        """
+        Function which calculate penalty value, it is threshold which decide if information gain is big enough to
+                create new Node, if not Leaf is created
+        :param current_depth: depth of concrete node
+        :return: threshold value
+        """
+        return THRESHOLD*(self.nodes_count/self.nodes_max + current_depth/self.depth_max)/2
 
     def evaluate(self, examples: np.ndarray, classes: np.ndarray):
         # TODO find solution with datatype of classes, best is int64 but sometimes it can failed if classes are not numbers
-        classes = classes.astype(np.dtype("object"))
-        predicted_classes = np.zeros(shape=classes.shape, dtype=np.dtype("object"))
+        wanted_type = classes.dtype
+        # classes = classes.astype(np.dtype("object"))
+        predicted_classes = np.zeros(shape=classes.shape, dtype=np.dtype(wanted_type))
         for i, example in enumerate(examples):
             label = self.predict(example)
             predicted_classes[i] = label
         conf_matrix = confusion_matrix(classes, predicted_classes)
         accuracy = accuracy_score(classes, predicted_classes)
+        report = classification_report(classes, predicted_classes)
         print("confusion matrix:")
         print(conf_matrix)
         print("accuracy:")
         print(accuracy)
+        print("classification report:")
+        print(report)
 
     # TODO
     def metrics(self):
@@ -237,7 +277,7 @@ class Leaf:
 dataset_path = "/home/stepi2299/studia/test.csv"
 dataset_path1 = "/home/stepi2299/studia/WSI/breast-cancer.csv"
 dataset_path2 = "/home/stepi2299/studia/WSI/agaricus-lepiota.csv"
-dataset = pd.read_csv(dataset_path)
+dataset = pd.read_csv(dataset_path1)
 tree = Tree()
-tree.train(dataset)
+tree.train(dataset, class_idx=-1, id_idx=None)
 
